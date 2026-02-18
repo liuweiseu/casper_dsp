028014216a758f8c6cb303d6781a335856f87b3f
Wei Liu <liuwei_berkeley@berkeley.edu>
2026-02-17 21:30:44 -0800

    run simulation with differnt paramters for the same module.

--- testbench/FlowControl/bus_create/test_bus_create_1.py
+++ testbench/FlowControl/bus_create/test_bus_create_1.py
@@ -8,12 +8,14 @@ import numpy as np
 # testdir is used for getting the path for the input and output data file
 testdir = Path(__file__).resolve().parent
 # load the input and expected output data 
-sim_in1 = np.loadtxt(testdir/'simdata0/sim_in1.csv', dtype=int).tolist()
-sim_in2 = np.loadtxt(testdir/'simdata0/sim_in2.csv', dtype=int).tolist()
-expected_results = np.loadtxt(testdir/'simdata0/sim_out.csv', dtype=int).tolist()
+sim_in1 = np.loadtxt(testdir/'simdata1/sim_in1.csv', dtype=int).tolist()
+sim_in2 = np.loadtxt(testdir/'simdata1/sim_in2.csv', dtype=int).tolist()
+sim_in3 = np.loadtxt(testdir/'simdata1/sim_in3.csv', dtype=int).tolist()
+sim_in4 = np.loadtxt(testdir/'simdata1/sim_in4.csv', dtype=int).tolist()
+expected_results = np.loadtxt(testdir/'simdata1/sim_out.csv', dtype=np.int64).tolist()
 
 @cocotb.test()
-async def bus_create_test_0(dut):
+async def bus_create_test_1(dut):
     """test bus_create """
     nbits = int(dut.NBITS.value)
     ninputs = int(dut.NINPUTS.value)
@@ -21,7 +23,7 @@ async def bus_create_test_0(dut):
     cocotb.log.info(f"Testing with NBITS={nbits}, NINPUTS={ninputs}")
 
     for i in range(len(sim_in1)):
-        dut.din.value = [sim_in2[i], sim_in1[i]]
+        dut.din.value = [sim_in4[i], sim_in3[i], sim_in2[i], sim_in1[i]]
         # wait for the logic to be stable
         await Timer(1, units="ns")
         # get the output
